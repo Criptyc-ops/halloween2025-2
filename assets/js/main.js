5022346ab2d151e9aab304d2b6dc455b76cbfa38
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-10-09 14:46:10 +0000

    habble

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ const scrollHeader = () => {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     this.scrollY >= 50 ? header.classList.add('scroll-header')
-                        : head3er.classlist.remove('scroll-header')
+                       : header.classlist.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
